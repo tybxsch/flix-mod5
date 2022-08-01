eba9845f408752265dd5996964b393ac1d3146d2
devdudd <tybuscheduarda@gmail.com>
2022-08-01 10:40:53 -0300

    Inicio do Header/Routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom'
+import Header from './components/Header/Header'
 import Home from './pages/Home/Home'
 
 const Routes = () => {
   return (
     <BrowserRouter>
+    <Header />
     <Switch>
 
         <Route path="/" element={<Home />}/>
