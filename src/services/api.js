dde4e191c39e78a5509775ee3e9e1a2dcecd9c41
devdudd <tybuscheduarda@gmail.com>
2022-08-05 09:48:45 -0300

    iniciei a requisicao

---
+++ src/services/api.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://devfli.herokuapp.com"
+});
+
+export const getSeries = async () => {
+    const response = await instance.get("/series");
+    const json = await response.data.series;
+    return json;
+};
+
+export const getSeriesById = async (id) => {
+    const response = await instance.get(`/series/${id}`);
+    const json = await response.data.series;
+    return json;
+};
+
+export const postSeries = async (body) => {
+    const response = await instance.post("/series/novaSerie", body);
+    const json = await response.data.msg;
+    return json;
+};
+
+export const editSeries = async (id, body) => {
+    const response = await instance.patch(`/series/${id}`, body);
+};
+
+export const deleteSeries = async (id) => {
+    const responde = await instance.delete(`/series/${id}`);
+};
