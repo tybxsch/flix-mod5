aa8ce7219ec24168a3bf9af9771f74dc7d7793df
Carol Saint <santocarolina98@gmail.com>
2022-08-09 21:33:39 -0300

    card button fixed

--- src/components/CardSerie/CardSerie.jsx
+++ src/components/CardSerie/CardSerie.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 import S from '../CardSerie/cardSerie.module.css'
+import Botao from '../Botao/Botao'
 
 const CardSerie = ({ title, urlimg }) => {
   return (
@@ -10,7 +11,7 @@ const CardSerie = ({ title, urlimg }) => {
       
       {/* <img src='https://br.web.img2.acsta.net/r_1280_720/pictures/14/04/14/10/51/458918.jpg'/>
       <h3>Club das Winx</h3> */}
-      //button
+      <Botao text='Assistir'/>
     </div>
   )
 }
