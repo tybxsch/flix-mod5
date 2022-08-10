6d1aacc56958181ccfdff9d8fbd7e8517873a735
devdudd <tybuscheduarda@gmail.com>
2022-08-09 21:09:25 -0300

    carrossel ok

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,13 +4,26 @@ import { getSeries } from '../../services/api';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import MovieRow from '../../components/MovieRow/MovieRow.jsx';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import S from '../Home/Home.module.css'
-// import test from 'https://www.nerdsite.com.br/wp-content/uploads/2019/05/mrorbot.jpg'
 
 
 const Home = () => {
   const params = useParams();
   const [series, setSeries] = useState([]);
+  const [scrollX, setScrollX] = useState(0);
+
+   const handlerLeftArrow = () => {
+    let x = scrollX + 150;
+    
+    setScrollX(x);
+   };
+
+   const handlerRightArrow = () => {
+    let x = scrollX - 150;
+    setScrollX(x);
+   };
 
  const require = async () => {
   const response = await getSeries();
@@ -36,8 +49,17 @@ const Home = () => {
       <div className={S.destaque}>
          <h2>Em destaque</h2>
       </div>
-      <div className={S.agoravai}>
-      <div className={S.catalogoHome}>        
+      <div className={S.movieRowLeft} onClick={handlerLeftArrow}>
+        <ArrowCircleLeftIcon/>
+      </div>
+      <div className={S.movieRowRight} onClick={handlerRightArrow}>
+        <ArrowCircleRightIcon/>
+      </div>
+      <div className={S.overflow}>
+      <div className={S.catalogoHome} style={
+        {marginLeft: scrollX,
+          width: series.length * 150
+        }}>        
         {!!series && series.map((serie) =>{
           return (<MovieRow
             urlimg={serie.urlimg}
